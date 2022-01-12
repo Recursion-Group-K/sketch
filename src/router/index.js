c271c490526d671dfba49b93a6ddef2fd1f63930
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-11 17:44:37 -0800

    add login page route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import VueRouter from 'vue-router';
 import Home from '../pages/Home.vue';
+import Login from '../pages/Login.vue';
 import NotFound from '../pages/NotFound.vue';
 
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
     },
     {
         path: '/:pathMatch(.*)*',
