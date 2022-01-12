93193fc77a1a947f73d05853a9c74f1597a6bb41
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-11 18:46:30 -0800

    add title

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,49 +1,59 @@
 <template>
     <section class="hero is-primary is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <div class="columns is-centered">
-          <div class="column is-5-tablet is-4-desktop is-4-widescreen">
-            <form action="" class="box">
-              <div class="title">
-                <h1 class="has-text-black has-text-weight-bold">Login</h1>
-              </div>
-              <hr>
-              <div class="field">
-                <label for="" class="label">Email</label>
-                <div class="control has-icons-left">
-                  <input type="email" placeholder="e.g. bobsmith@gmail.com" class="input" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
-                  </span>
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+                    <div class="column is-6">
+                        <h1 class="page-title title is-1">Etch A Sketch</h1>
+                        <p class="subtitle">Unlock your imagination</p>
+                    </div>
+                    <div class="column is-5-tablet is-4-desktop is-4-widescreen">
+                        <form action="" class="box">
+                            <div class="title">
+                                <h1 class="has-text-black has-text-weight-bold">Login</h1>
+                            </div>
+                            <hr />
+                            <div class="field">
+                                <label for="" class="label">Email</label>
+                                <div class="control has-icons-left">
+                                    <input
+                                        type="email"
+                                        placeholder="e.g. bobsmith@gmail.com"
+                                        class="input"
+                                        required
+                                    />
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label for="" class="label">Password</label>
+                                <div class="control has-icons-left">
+                                    <input
+                                        type="password"
+                                        placeholder="*******"
+                                        class="input"
+                                        required
+                                    />
+                                    <span class="icon is-small is-left">
+                                        <i class="fa fa-lock"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <button class="button is-success">Login</button>
+                            </div>
+                            <hr />
+                            <div class="field">
+                                <button class="button is-success">Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-              <div class="field">
-                <label for="" class="label">Password</label>
-                <div class="control has-icons-left">
-                  <input type="password" placeholder="*******" class="input" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
-                  </span>
-                </div>
-              </div>
-              <div class="field">
-                <button class="button is-success">
-                  Login
-                </button>
-              </div>
-              <hr>
-              <div class="field">
-                <button class="button is-success">
-                  Sign Up
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <style scoped lang="scss"></style>
@@ -52,10 +62,14 @@
 export default {
     name: 'Login',
     data() {
-        return {
-            
-        };
+        return {};
     },
 };
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+.page-title {
+    font-family: 'Lobster', cursive;
+}
+</style>
