833490815b2d1946ba13eabffcff9b10e30f7a7e
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-11 17:43:55 -0800

    create login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="hero is-primary is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-5-tablet is-4-desktop is-4-widescreen">
+            <form action="" class="box">
+              <div class="title">
+                <h1 class="has-text-black has-text-weight-bold">Login</h1>
+              </div>
+              <hr>
+              <div class="field">
+                <label for="" class="label">Email</label>
+                <div class="control has-icons-left">
+                  <input type="email" placeholder="e.g. bobsmith@gmail.com" class="input" required>
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <label for="" class="label">Password</label>
+                <div class="control has-icons-left">
+                  <input type="password" placeholder="*******" class="input" required>
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-lock"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <button class="button is-success">
+                  Login
+                </button>
+              </div>
+              <hr>
+              <div class="field">
+                <button class="button is-success">
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            
+        };
+    },
+};
+</script>
+
