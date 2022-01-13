ad957d3812c7ede0b07a90eabedede37d7eda6ed
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-13 01:58:15 -0800

    add userWrapper.js

---
+++ src/api/userWrapper.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import User from '../models/user.js';
+
+export default class UserWrapper {
+  url = "https://jsonplaceholder.typicode.com"
+  constructor(){}
+
+  async get(id){
+    try{
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      const params = {id: response.data.id, name: response.data.name, email: response.data.email, password: response.data.username}
+      return new User(params);
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+}
