0eef6b791b4995c596566e3242bf19889cb58b5c
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-13 16:26:27 +0000

    add drawing.js

---
+++ src/models/drawing.js
@@ -0,0 +1,11 @@
+export class Drawing {
+    constructor(params) {
+        this.id = params.id; // string
+        this.title = params.title; //string
+        this.imgUrl = params.imgUrl; // string
+        this.data = params.data; // json
+        this.created_at = params.created_at; // datetime
+        this.updated_at = params.updated_at; // datetime
+        this.userId = params.userId; // integer
+    }
+}
