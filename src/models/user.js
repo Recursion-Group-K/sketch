92520e52e83247fe27ebcf0407026acee700e795
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-13 01:58:39 -0800

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,8 @@
+export default class User {
+  constructor(params){
+    this.id = params.id;
+    this.name = params.name;
+    this.email = params.email;
+    this.password = params.password;
+  }
+}
