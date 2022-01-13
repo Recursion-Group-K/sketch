fda1ad3eebef383a649f8c1c06bbf2273cc8c46b
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-13 22:58:12 +0000

    add drawingWrapper.js

---
+++ src/api/drawingWrapper.js
@@ -0,0 +1,10 @@
+import Drawing from "../models/drawing";
+
+export default class DrawingWapper {
+    url = "http://localhost:3000/drawings";
+    constructor(){}
+    
+    getDrawingById(id) {
+
+    }
+}
