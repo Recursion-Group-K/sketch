292c58d341ea68b171567582968a89bd7e4c24c7
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-14 16:12:39 -0800

    Revert "add test/unit/userWrapperTest.spec.js and api/userWrapper.js"
    This reverts commit f63452455f6de182c8514a3abb13629414c659b1.

--- tests/unit/userWrapperTest.spec.js
+++
@@ -1,9 +0,0 @@
-import UserWrapper from '../../src/api/userWrapper.js';
-
-describe('UserWrapper', () => {
-    it('get User by id:1', async () => {
-        const user1 = await new UserWrapper().get(1);
-
-        console.log(user1);
-    });
-});
