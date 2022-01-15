f63452455f6de182c8514a3abb13629414c659b1
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-14 16:12:26 -0800

    add test/unit/userWrapperTest.spec.js and api/userWrapper.js

--- src/api/userWrapper.js
+++ src/api/userWrapper.js
@@ -1,18 +1,22 @@
-import axios from 'axios'
+import axios from 'axios';
 import User from '../models/user.js';
 
 export default class UserWrapper {
-  url = "https://jsonplaceholder.typicode.com"
-  constructor(){}
+    url = 'https://jsonplaceholder.typicode.com';
+    constructor() {}
 
-  async get(id){
-    try{
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      const params = {id: response.data.id, name: response.data.name, email: response.data.email, password: response.data.username}
-      return new User(params);
+    async get(id) {
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const params = {
+                id: response.data.id,
+                name: response.data.name,
+                email: response.data.email,
+                password: response.data.username,
+            };
+            return new User(params);
+        } catch (error) {
+            console.log(error);
+        }
     }
-    catch(error) {
-      console.log(error)
-    }
-  }
 }
---
+++ tests/unit/userWrapperTest.spec.js
@@ -0,0 +1,9 @@
+import UserWrapper from '../../src/api/userWrapper.js';
+
+describe('UserWrapper', () => {
+    it('get User by id:1', async () => {
+        const user1 = await new UserWrapper().get(1);
+
+        console.log(user1);
+    });
+});
