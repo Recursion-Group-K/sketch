2ac8d6854ffd53e06cc474fbac5979eee17463e8
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-15 23:56:09 +0000

    add new param in drawing model

--- src/models/drawing.js
+++ src/models/drawing.js
@@ -1,8 +1,9 @@
-export class Drawing {
+export default class Drawing {
     constructor(params) {
         this.id = params.id; // string
         this.title = params.title; //string
         this.imgUrl = params.imgUrl; // string
+        this.isPublic = params.isPublic // boolean
         this.data = params.data; // json
         this.created_at = params.created_at; // datetime
         this.updated_at = params.updated_at; // datetime
