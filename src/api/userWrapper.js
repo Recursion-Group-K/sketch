c94cdb2dd6308e9b166c53f7aa65289439fc0bd9
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-15 23:23:01 -0800

    分割代入を使用してDRYを行いました

--- src/api/userWrapper.js
+++ src/api/userWrapper.js
@@ -1,29 +1,21 @@
 import axios from 'axios';
-<<<<<<< HEAD
 import User from '../models/user.js';
-=======
-// import User from '../models/user.js';
->>>>>>> 512bf31d415728e1a691f10305c4f5b915edb362
 
 export default class UserWrapper {
     url = 'https://jsonplaceholder.typicode.com';
     constructor() {}
 
-    async getById(id) {
+    async getById(userId) {
         try {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-            const params = {
-                id: response.data.id,
-                name: response.data.name,
-                email: response.data.email,
-                password: response.data.username,
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+            const { id, name, email, username } = response.data;
+            const param = {
+                id: id,
+                name: name,
+                email: email,
+                password: username,
             };
-<<<<<<< HEAD
-            return new User(params);
-=======
-            return params;
-            // return new User(params);
->>>>>>> 512bf31d415728e1a691f10305c4f5b915edb362
+            return new User(param);
         } catch (error) {
             console.log(error);
         }
