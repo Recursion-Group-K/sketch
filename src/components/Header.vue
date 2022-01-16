de6eff1898739abc1fa2923b1045916631293504
sokahiri <sokahi777@gmail.com>
2022-01-17 08:29:24 +0900

    fontawesomeのインストール、ヘッダーのデザインの変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,25 @@
+<style>
+.header-drowing{
+    height: 4.5rem;
+}
+</style>
+
 <template>
     <div class="hero is-link is-tablet">
-        <div class="column">
-            <nav class="level is-mobile is-primary">
+        <div class="column py-0">
+            <nav class="level is-mobile is-primary header-drowing">
                 <div class="level-left">
                     <div class="level-item is-hidden-mobile has-text-weight-bold pr-5">
                         title of work
                     </div>
-                    <div class="level-item">color</div>
-                    <div class="level-item">pixel</div>
-                    <div class="level-item">redo</div>
-                    <div class="level-item">undo</div>
+                    <div class="level-item is-flex-direction-column-reverse"><span class=" is-size-7">color</span><font-awesome-icon class="is-size-4" icon="palette" /></div>
+                    <div class="level-item is-flex-direction-column-reverse"><span class=" is-size-7">pixel</span><font-awesome-icon class="is-size-4" icon="circle" /></div>
+                    <div class="level-item is-flex-direction-column-reverse"><span class=" is-size-7">redo</span><font-awesome-icon class="is-size-4" icon="redo" /></div>
+                    <div class="level-item is-flex-direction-column-reverse"><span class=" is-size-7">undo</span><font-awesome-icon class="is-size-4" icon="undo" /></div>
                 </div>
                 <div class="level-right">
-                    <div class="level-item is-hidden-mobile">save</div>
-                    <div class="level-item is-hidden-mobile">share</div>
+                    <div class="level-item is-hidden-mobile is-flex-direction-column-reverse"><span class=" is-size-7">save</span><font-awesome-icon class="is-size-4" icon="save" /></div>
+                    <div class="level-item is-hidden-mobile is-flex-direction-column-reverse"><span class=" is-size-7">share</span><font-awesome-icon class="is-size-4" icon="share-square" /></div>
                     <div class="level-item is-hidden-tablet">
                         <div class="dropdown is-right" 
                             v-bind:class="{'is-active':isActive}" 
@@ -21,25 +27,23 @@
                         >
                             <div class="dropdown-trigger">
                                 <button class="button is-link">
-                                    <span>others</span>
+                                    <div class="is-flex is-flex-direction-column-reverse is-justify-content-center has-text-centered"><span class=" is-size-7">others</span><span><font-awesome-icon class="is-size-4" icon="bars" /></span></div>
                                 </button>
                             </div>
                             <div class="dropdown-menu">
                                 <div class="dropdown-content">
-                                    <div class="dropdown-item">save</div>
-                                    <div class="dropdown-item">share</div>
+                                    <div class="dropdown-item">save<font-awesome-icon icon="save" /></div>
+                                    <div class="dropdown-item">share<font-awesome-icon icon="share-square" /></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="level-item">back</div>
+                    <div class="level-item is-flex-direction-column-reverse"><span class=" is-size-7">back</span><font-awesome-icon class="is-size-4" icon="door-open" /></div>
                 </div>
             </nav>
         </div>
     </div>
 </template>
-
-<style scoped lang="scss"></style>
 
 <script>
 export default {
@@ -51,5 +55,3 @@ export default {
     },
 };
 </script>
-
-<style></style>
--- src/pages/Drawing.vue
+++ src/pages/Drawing.vue
@@ -1,11 +1,34 @@
+<style>
+.drowing-container{
+    height: calc(100vh - 4.5rem);
+}
+.button-container{
+    height: 20%;
+}
+.canvas-container {
+    height: 85%;
+    width: 100vw;
+}
+.move-button {
+    height: 4rem;
+    width: 4rem;
+    margin: 0.25rem;
+}
+@media (min-width: 1024px) {
+    .canvas-container {
+        height: 95%;
+    }
+}
+</style>
+
 <template>
     <div>
         <Header/>
-        <div class="hero is-primary is-fullheight-with-navbar">
-            <div class="py-5">
-                <canvas class="canvas-size"></canvas>
+        <div class="has-background-primary drowing-container d-flex is-flex-direction-column">
+            <div class="py-0 canvas-container has-background-grey">
+                <canvas class="has-background-white"></canvas>
             </div>
-            <div class="is-flex is-justify-content-space-between pb-3">
+            <div class="is-flex is-justify-content-space-between button-container pb-3">
                 <div class="is-hidden-desktop px-4">
                     <button class="move-button">up</button>
                     <button class="move-button">down</button>
@@ -20,8 +43,6 @@
     </div>
 </template>
 
-<style scoped lang="scss"></style>
-
 <script>
 import Header from '../components/Header.vue';
 
@@ -33,21 +54,3 @@ export default {
     },
 };
 </script>
-
-<style>
-.canvas-size {
-    height: 80vh;
-    width: 100vw;
-    background-color: white;
-}
-.move-button {
-    height: 4rem;
-    width: 4rem;
-    margin: 0.25rem;
-}
-@media (min-width: 1024px) {
-    .canvas-size {
-        height: calc(80vh + 4rem);
-    }
-}
-</style>
