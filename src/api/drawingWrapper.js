546446a6671b8d8d638a7b3e9224c2b6ea5cc12b
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-19 12:39:12 +0000

    refactor DrawingWrapper

--- src/api/drawingWrapper.js
+++ src/api/drawingWrapper.js
@@ -2,6 +2,31 @@ import Drawing from '../models/drawing';
 
 const axios = require('axios');
 
+function toParams(drawingData) { // object
+    const {
+        id: drawingId,
+        title,
+        imgUrl,
+        isPublic,
+        data,
+        createdAt,
+        updatedAt,
+        userId,
+    } = drawingData;
+    const params = {
+        id: drawingId,
+        title: title,
+        imgUrl: imgUrl,
+        isPublic: isPublic,
+        data: data,
+        createdAt: new Date(createdAt),
+        updatedAt: new Date(updatedAt),
+        userId: userId,
+    };
+
+    return params;
+}
+
 export default class DrawingWapper {
     url = 'http://localhost:3000/drawings';
     constructor() {}
@@ -9,26 +34,7 @@ export default class DrawingWapper {
     async getById(id) { // return Drawing
         try {
             const response = await axios.get(`${this.url}/${id}`);
-            const {
-                id: drawingId,
-                title,
-                imgUrl,
-                isPublic,
-                data,
-                createdAt,
-                updatedAt,
-                userId,
-            } = response.data;
-            const params = {
-                id: drawingId,
-                title: title,
-                imgUrl: imgUrl,
-                isPublic: isPublic,
-                data: data,
-                createdAt: createdAt,
-                updatedAt: updatedAt,
-                userId: userId,
-            };
+            const params = toParams(response.data);
             return new Drawing(params);
         } catch (error) {
             console.error(error);
@@ -41,26 +47,7 @@ export default class DrawingWapper {
             const drawingsArray = response.data
             let drawings = [];
             for (const drawing of drawingsArray) {
-                const {
-                    id: drawingId,
-                    title,
-                    imgUrl,
-                    isPublic,
-                    data,
-                    createdAt,
-                    updatedAt,
-                    userId,
-                } = drawing;
-                const params = {
-                    id: drawingId,
-                    title: title,
-                    imgUrl: imgUrl,
-                    isPublic: isPublic,
-                    data: data,
-                    createdAt: createdAt,
-                    updatedAt: updatedAt,
-                    userId: userId,
-                };
+                const params = toParams(drawing);
                 const newDrawing = new Drawing(params);
                 drawings.push(newDrawing);
             }
@@ -76,26 +63,7 @@ export default class DrawingWapper {
             const drawingsArray = response.data
             let drawings = [];
             for (const drawing of drawingsArray) {
-                const {
-                    id: drawingId,
-                    title,
-                    imgUrl,
-                    isPublic,
-                    data,
-                    createdAt,
-                    updatedAt,
-                    userId,
-                } = drawing;
-                const params = {
-                    id: drawingId,
-                    title: title,
-                    imgUrl: imgUrl,
-                    isPublic: isPublic,
-                    data: data,
-                    createdAt: createdAt,
-                    updatedAt: updatedAt,
-                    userId: userId,
-                };
+                const params = toParams(drawing);
                 const newDrawing = new Drawing(params);
                 drawings.push(newDrawing);
             }
