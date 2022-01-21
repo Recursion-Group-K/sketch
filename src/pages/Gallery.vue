5df3d1126359c6fadfaefa245aa79653ea4704c2
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-21 17:27:52 +0000

    画像表示部分

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,5 +1,160 @@
 <template>
-    <div>Hello</div>
+    <div id="Gallery" class="">
+        <!-- <div id="header">
+
+        </div> -->
+
+        <h1 class="title has-text-centered my-4">Gallery</h1>
+
+        <div id="showcase" class="is-flex is-justify-content-center">
+            <div class="box column is-four-fifths is-flex is-justify-content-space-between is-flex-wrap-wrap p-6" style="height: 85vh">
+                <div class="card mb-4" style="width: 240px">
+                    
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img">
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer"> 
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+
+                <div class="card mb-4" style="width: 240px">
+                    <div class="card-img" >
+                        <img src="https://image.rakuten.co.jp/asiantique/cabinet/03189910/img63361868.jpg" alt="...">
+                    </div>
+                    <div class="p-1 has-text-centered">
+                        <p class="title is-6">Flower Garden</p>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item">Share</a>
+                        <a class="card-footer-item">Edit</a>
+                        <a class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,3 +165,14 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+    .box {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        overflow: scroll;
+    }
+
+    .border {
+        border: solid;
+    }
+</style>
