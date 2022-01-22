3c0c1849033890c2ad74760af2782140dea66a55
Leandro <leandro83g.development@gmail.com>
2022-01-22 18:26:09 +0900

    feat: delete Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import VueRouter from 'vue-router';
-import Home from '../pages/Home.vue';
 import Drawing from '../pages/Drawing.vue';
 import Login from '../pages/Login.vue';
 import SignUp from '../pages/SignUp.vue';
@@ -9,7 +8,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home,
+        component: Login,
     },
     {
         path: '/login',
