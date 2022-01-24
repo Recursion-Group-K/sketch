3a603e8f34ee91ead01da458ae5a8bd5dd6e71a0
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-24 20:54:02 +0000

    add new icons

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import store from './store';
 import VueRouter from 'vue-router';
 import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faCloud, faEdit, faTrash, faBookReader } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(faCloud, faEdit, faTrash, faBookReader)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
