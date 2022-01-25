33a4cae70aaa5a8a8c9a607f9c9785326a292527
sokahiri <sokahi777@gmail.com>
2022-01-26 07:04:37 +0900

    リファクタリング

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -12,6 +12,12 @@
 
 <script>
 const velocityOfPointer = 2;
+const keyMap = {
+    'd':"isUp",
+    'f':"isDown",
+    'k':"isRight",
+    'j':"isLeft",
+}
 let canvasWidth=window.innerWidth;
 let canvasHeight=500;
 
@@ -37,62 +43,48 @@ export default {
                 lineCap: 'round',
                 points:[canvasWidth/2,canvasHeight/2]
             },
-            controller:{
-                x:{isPositive:false, isNegative:false},
-                y:{isPositive:false, isNegative:false},
-                timer:undefined,
+            direction:{
+                isUp:false,
+                isDown:false,
+                isRight:false,
+                isLeft:false,
             },
+            timer:undefined,
         }
     },
     mounted: function(){
-        this.controller.x={isPositive:false, isNegative:false};
-        this.controller.y={isPositive:false, isNegative:false};
+        this.direction.isUp=false;
+        this.direction.isDown=false;
+        this.direction.isRight=false;
+        this.direction.isLeft=false;
         document.addEventListener('keydown',this.keyDown);
         document.addEventListener('keyup',this.keyUp);
-        this.controller.timer=setInterval(this.draw,10);
+        this.timer=setInterval(this.draw,10);
     },
     destroyed: function(){
         document.removeEventListener('keydown',this.keyDown);
         document.removeEventListener('keyup',this.keyUp);
-        clearInterval(this.controller.timer);
+        clearInterval(this.timer);
     },
     methods: {
-        keyDown(event){
+        keyEvent(event,boolean){
             let key=event.key;
-            if(key=="d"){
-                this.controller.y.isPositive=true;
-            }
-            if(key=="f"){
-                this.controller.y.isNegative=true;
-            }
-            if(key=="k"){
-                this.controller.x.isPositive=true;
-            }
-            if(key=="j"){
-                this.controller.x.isNegative=true;
+            if(key in keyMap){
+                this.direction[keyMap[key]]=boolean;
             }
         },
+        keyDown(event){
+            this.keyEvent(event,true);
+        },
         keyUp(event){
-            let key=event.key;
-            if(key=="d"){
-                this.controller.y.isPositive=false;
-            }
-            if(key=="f"){
-                this.controller.y.isNegative=false;
-            }
-            if(key=="k"){
-                this.controller.x.isPositive=false;
-            }
-            if(key=="j"){
-                this.controller.x.isNegative=false;
-            }
+            this.keyEvent(event,false);
         },
         draw(){
             let lastPoint=[this.pointer.x,this.pointer.y];
-            if(this.controller.y.isPositive==true)this.pointer.y+=velocityOfPointer;
-            if(this.controller.y.isNegative==true)this.pointer.y-=velocityOfPointer;
-            if(this.controller.x.isPositive==true)this.pointer.x+=velocityOfPointer;
-            if(this.controller.x.isNegative==true)this.pointer.x-=velocityOfPointer;
+            if(this.direction["isUp"])this.pointer.y-=velocityOfPointer;
+            if(this.direction["isDown"])this.pointer.y+=velocityOfPointer;
+            if(this.direction["isRight"])this.pointer.x+=velocityOfPointer;
+            if(this.direction["isLeft"])this.pointer.x-=velocityOfPointer;
             if(lastPoint[0]!=this.pointer.x||lastPoint[1]!=this.pointer.y){
                 this.lineConfig.points.push(this.pointer.x,this.pointer.y);
             }
