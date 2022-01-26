5a7a503c7cf4e2145331278c381f236d0e75e1c0
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-26 12:20:56 +0000

    modify userWrapper.js

--- src/api/userWrapper.js
+++ src/api/userWrapper.js
@@ -9,10 +9,10 @@ export default class UserWrapper {
     async getById(id) {
         try {
             const response = await axios.get(`${this.url}/users/${id}`);
-            const { id: userId, name, isActive: is_active } = response.data;
+            const { id: userId, username, is_active } = response.data;
             const params = {
                 id: userId,
-                name: name,
+                name: username,
                 isActive: is_active
             };
             return new User(params);
