b9d023696837e5da3a4085f999ba316b767ccd3f
Leandro <leandro83g.development@gmail.com>
2022-01-26 17:24:04 +0900

    lnit

--- src/components/DrawingBox.vue
+++ src/components/DrawingBox.vue
@@ -11,38 +11,45 @@
         </figure>
         <p class="title is-5 has-text-dark">{{ drawing.title }}</p>
         <div class="column is-one-fifths is-flex is-justify-content-flex-end">
-            <font-awesome-icon icon="globe-asia"
+            <font-awesome-icon
+                icon="globe-asia"
                 :class="{ 'has-text-success': isPublic }"
                 class="mx-1 awesome-icon"
                 size="lg"
                 @click="toggoleIsPublic"
             />
-            <font-awesome-icon :icon="['fab', 'twitter']"
-                class="mx-1 awesome-icon has-text-info" 
+            <font-awesome-icon
+                :icon="['fab', 'twitter']"
+                class="mx-1 awesome-icon has-text-info"
                 size="lg"
                 @click="twitterShare"
             />
-            <font-awesome-icon icon="trash"
-                class="mx-1 awesome-icon has-text-danger" 
+            <font-awesome-icon
+                icon="trash"
+                class="mx-1 awesome-icon has-text-danger"
                 size="lg"
                 @click="handleDeleteConfirmationModal(true)"
             />
         </div>
 
         <!-- Delete Confirmation Modal -->
-        <div class="modal" :class="{'is-active': isOpenDeleteConfirmationModal}">
+        <div class="modal" :class="{ 'is-active': isOpenDeleteConfirmationModal }">
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">Delete Confirmation</p>
-                    <button class="delete" aria-label="close" @click="handleDeleteConfirmationModal(false)"></button>
+                    <button
+                        class="delete"
+                        aria-label="close"
+                        @click="handleDeleteConfirmationModal(false)"
+                    ></button>
                 </header>
-                <section class="modal-card-body">
-                    Are you sure you want delete the Drawing.
-                </section>
+                <section class="modal-card-body">Are you sure you want delete the Drawing.</section>
                 <footer class="modal-card-foot is-flex is-justify-content-center">
                     <button class="button is-danger" @click="deleteDrawing">Delete</button>
-                    <button class="button" @click="handleDeleteConfirmationModal(false)">Cancel</button>
+                    <button class="button" @click="handleDeleteConfirmationModal(false)">
+                        Cancel
+                    </button>
                 </footer>
             </div>
         </div>
@@ -66,14 +73,14 @@ export default {
             this.isPublic = !this.isPublic;
         },
         twitterShare() {
-            console.log('gggg')
+            console.log('gggg');
         },
         handleDeleteConfirmationModal(bool) {
-            this.isOpenDeleteConfirmationModal = bool
+            this.isOpenDeleteConfirmationModal = bool;
         },
         deleteDrawing() {
-            console.log('gggg')
-            this.handleDeleteConfirmationModal(false)
+            console.log('gggg');
+            this.handleDeleteConfirmationModal(false);
         },
     },
 };
