1474dc01e45a95d64737820f38a1d447cf6d5191
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-27 20:41:04 +0000

    axios basic auth

--- src/api/userWrapper.js
+++ src/api/userWrapper.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import User from '../models/user.js';
 
+require('dotenv').config();
+
 export default class UserWrapper {
     constructor() {
-        this.url = 'https://jsonplaceholder.typicode.com';
+        this.url = 'https://sketch-skgl-server-test.herokuapp.com/api';
     }
 
     async getById(id) {
         try {
-            const response = await axios.get(`${this.url}/users/${id}`);
+            const response = await axios.get(`${this.url}/users/${id}/`, {
+                auth: {
+                    username: process.env.SUPERUSER_NAME,
+                    password: process.env.SUPERUSER_PASSWORD
+                }
+            });
             const { id: userId, username, is_active } = response.data;
             const params = {
                 id: userId,
