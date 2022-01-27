b14b34f32d871aa8f79cc86a41d872579ac5b3dd
Jin2oo2 <nojinhiroaki@icloud.com>
2022-01-27 20:38:47 +0000

    add test for userWrapper

--- tests/unit/userWrapperTest.spec.js
+++ tests/unit/userWrapperTest.spec.js
@@ -6,4 +6,16 @@ describe('UserWrapper', () => {
 
         console.log(user1);
     });
+
+    it('get User by id:2', async () => {
+        const user2 = await new UserWrapper().getById(2);
+
+        console.log(user2);
+    });
+
+    it('get User by id:3', async () => {
+        const user3 = await new UserWrapper().getById(3);
+
+        console.log(user3);
+    });
 });
