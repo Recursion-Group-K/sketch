78dc31a5bc71b7cfcf6525ae5238b9c92ad7536d
Taesok Kwon <kwontaesok1997@gmail.com>
2022-02-03 13:16:51 -0800

    create signup action and mutations

--- src/store/signup.js
+++ src/store/signup.js
@@ -1,24 +1,18 @@
 import UserWrapper from '../api/userWrapper';
-import client from '../api/client';
 
 import {
-    SET_TOKEN,
     SIGNUP_BEGIN,
     SIGNUP_FAILURE,
     SIGNUP_SUCCESS,
-    SET_TOKEN2,
 }
 from './types'
-import Cookies from 'js-cookie';
-const ACCESS_TOKEN_STORAGE_KEY = 'ACCESS_TOKEN_STORAGE_KEY';
-const REFRESH_TOKEN_STORAGE_KEY = 'REFRESH_STORAGE_KEY';
+
 
 export default {
     namespaced: true,
     state: {
         isLoading: false,
         hasError: false,
-        token: null,
     },
     actions: {
         async createAccount({ commit }, { username, email, password }) {
@@ -29,8 +23,7 @@ export default {
                 password: password,
             };
             try {
-                const response = await new UserWrapper().create(userParams);
-                commit(SET_TOKEN2, response.data);
+                await new UserWrapper().create(userParams);
                 commit(SIGNUP_SUCCESS);
             } catch (error) {
                 console.error(error);
@@ -49,12 +42,6 @@ export default {
         [SIGNUP_FAILURE](state) {
             state.isLoading = false;
             state.hasError = true;
-        },
-        [SET_TOKEN](state, token) {
-            Cookies.set(ACCESS_TOKEN_STORAGE_KEY, token.access);
-            Cookies.set(REFRESH_TOKEN_STORAGE_KEY, token.refresh);
-            client.defaults.headers.Authorization = `Bearer ${token.access}`;
-            state.token = token;
         },
     },
 };
