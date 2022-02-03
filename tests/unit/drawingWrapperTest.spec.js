834b24a0d1d0381d6083eefe0dfe7023319f0629
Leandro <72016706+mcnLeandro@users.noreply.github.com>
2022-02-03 13:55:03 +0900

    refactor: update drawingWrapper test

--- tests/unit/drawingWrapperTest.spec.js
+++ tests/unit/drawingWrapperTest.spec.js
@@ -3,25 +3,26 @@ import DrawingWapper from '../../src/api/drawingWrapper';
 describe('DrawingWrapperクラスのテスト', () => {
     test('getById', async () => {
         const drawing1 = await new DrawingWapper().getById(1);
-        console.log(drawing1);
+        expect(drawing1.id).toEqual(1);
     });
 
     test('getAll()', async () => {
         const allDrawings = await new DrawingWapper().getAll();
-        console.log(allDrawings);
+        expect(allDrawings).not.toBeNull();
+        expect(allDrawings).not.toBeUndefined();
     });
 
     test('getBy(column, data)', async () => {
         const drawing3 = await new DrawingWapper().getBy('id', 3);
-        console.log(drawing3);
+        expect(drawing3[0].id).toEqual(3);
 
         const drawingSheep = await new DrawingWapper().getBy('title', 'sheep');
-        console.log(drawingSheep);
+        expect(drawingSheep[0].title).toEqual('sheep');
 
         const drawingPublic = await new DrawingWapper().getBy('isPublic', true);
-        console.log(drawingPublic);
+        expect(drawingPublic[0].isPublic).toEqual(true);
 
         const drawingUser2 = await new DrawingWapper().getBy('userId', 2);
-        console.log(drawingUser2);
+        expect(drawingUser2[0].userId).toEqual(2);
     });
 });
